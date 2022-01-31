ab6b2bd65c075ba2c028be00e000731b6697b1b9
Ritwick Mitra <rmitra2u@gmail.com>
2022-02-01 01:09:00 +0530

    Test Cases enhancement

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerITest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerITest.java
@@ -15,39 +15,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
-public class BeerControllerITest {
+public class BeerControllerITest extends BaseIT{
 
-    @Autowired
-    WebApplicationContext context;
-
-    MockMvc mockMvc;
-
-    @MockBean
-    BeerRepository beerRepository;
-    @MockBean
-    BeerInventoryRepository beerInventoryRepository;
-    @MockBean
-    BreweryService breweryService;
-    @MockBean
-    CustomerRepository customerRepository;
-    @MockBean
-    BeerService beerService;
-
-    @BeforeEach
-    void setUp(){
-        this.mockMvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity())
-                .build();
-    }
-
-    @WithMockUser("anyName")
+    //@WithMockUser("anyName")
     @Test
     void findBeers() throws Exception{
         mockMvc.perform(get("/beers/find"))
@@ -59,6 +36,14 @@ public class BeerControllerITest {
     @Test
     void findBeersWithHttpBasic() throws Exception {
         mockMvc.perform(get("/beers/find").with(httpBasic("spring","password")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/findBeers"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    @Test
+    void findBeersWithoutSecurity() throws Exception {
+        mockMvc.perform(get("/beers/find").with(anonymous()))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
