5c1bfba0d2cb4ef4833e988935a42188a226899e
Ritwick Mitra <rmitra2u@gmail.com>
2022-02-02 23:17:30 +0530

    In Memory Authentication Provider Added

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,15 +1,23 @@
 package guru.sfg.brewery.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    // This method is used for customizing the authorization
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -26,4 +34,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
     }
+
+    // This method is used for customizing the authentication
+    // This will override the security credentials provided in application.properties
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("newAdmin")
+                .password("{noop}someAdminPass")
+                .roles("ADMIN")
+                .and()
+                .withUser("newUser")
+                .password("{noop}someUserPass")
+                .roles("USER");
+    }
+
+    // This is another way to provide In Memory Authentication Details
+    /* @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("newAdmin")
+                .password("someAdminPass")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("newUser")
+                .password("someUserPass")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }*/
 }
--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerITest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerITest.java
@@ -1,28 +1,23 @@
 package guru.sfg.brewery.web.controllers;
 
-import guru.sfg.brewery.repositories.BeerInventoryRepository;
-import guru.sfg.brewery.repositories.BeerRepository;
-import guru.sfg.brewery.repositories.CustomerRepository;
-import guru.sfg.brewery.services.BeerService;
-import guru.sfg.brewery.services.BreweryService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
-public class BeerControllerITest extends BaseIT{
+public class BeerControllerITest extends BaseIT {
+
+    @Test
+    void initCreationForm() throws Exception {
+        mockMvc.perform(get("/beers/new").with(httpBasic("newUser", "someUserPass")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
 
     //@WithMockUser("anyName")
     @Test
@@ -33,14 +28,17 @@ public class BeerControllerITest extends BaseIT{
                 .andExpect(model().attributeExists("beer"));
     }
 
+    // With the current SecurityConfig this /beers/find endpoint doesn't require any authorisation/authentication
     @Test
     void findBeersWithHttpBasic() throws Exception {
-        mockMvc.perform(get("/beers/find").with(httpBasic("spring","password")))
+        mockMvc.perform(get("/beers/find").with(httpBasic("newAdmin","someAdminPass")))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/findBeers"))
                 .andExpect(model().attributeExists("beer"));
     }
 
+    // calling with(anonymous()) and directly calling the endpoint is same
+    // anonymous() is used so that we can check any user is allowed to access the endpoint
     @Test
     void findBeersWithoutSecurity() throws Exception {
         mockMvc.perform(get("/beers/find").with(anonymous()))
